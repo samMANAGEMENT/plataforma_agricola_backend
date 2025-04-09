45acf8d1eef587446a37aa1306f15a63093a72af
Samuel Pineda <samuel.pineda@sumimedical.com>
2025-04-09 08:53:31 -0500

    first commit

---
+++ app/Http/Modules/Tipo/Models/Tipo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Modules\Tipo\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tipo extends Model
+{
+    use HasFactory;
+}
