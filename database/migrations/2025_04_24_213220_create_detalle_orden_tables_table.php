631742865c620656aabb7d6c01910c4d21e936f5
lSagenav <jcvanegas@unac.edu.co>
2025-04-24 16:37:41 -0500

    se crean las migraciones y los modelos de las tablas productos, ordenes y detalleOrden

---
+++ database/migrations/2025_04_24_213220_create_detalle_orden_tables_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('detalle_orden_tables', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('orden_id')->constrained('ordenes');
+            $table->foreignId('producto_id')->constrained('productos');
+            $table->decimal('cantidad', 10, );
+            $table->decimal('precio_unitario', 10, 3);
+            $table->decimal('subtotal', 10, 3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detalle_orden_tables');
+    }
+};
